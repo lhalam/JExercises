eda68a739beb21e1b0e3320ec409d0812ea1fc58
Kucheryavenko Dmytro <kucheryavenko.dmtr@gmail.com>
2015-01-20 17:57:07 +0200

    Add Assert,UserAnswer

---
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Assert.java
@@ -0,0 +1,37 @@
+package com.softserveinc.ita.jexercises.common.entity;
+
+import javax.persistence.*;
+
+@Table(name = "ASSERT")
+public class Assert extends BaseEntity {
+	@Column(name = QUESTION_ID)
+	private Question question;
+	@Column(name = EXPECTED_ANSWER)
+	private String expectedAnswer;
+	@Column(name = INPUT_DATA)
+	private String inputData;
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public String getExpectedAnswer() {
+		return expectedAnswer;
+	}
+
+	public void setExpectedAnswer(String expectedAnswer) {
+		this.expectedAnswer = expectedAnswer;
+	}
+
+	public String getInputData() {
+		return inputData;
+	}
+
+	public void setInputData(String inputData) {
+		this.inputData = inputData;
+	}
+}
---
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/UserAnswer.java
@@ -0,0 +1,39 @@
+package com.softserveinc.ita.jexercises.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+@Table(name="USER_ANSWER")
+public class UserAnswer extends BaseEntity {
+	@ Column(name=ATTEMPT)
+	private Attempt attempt;
+	@ Column(name=ANSWER)
+	private String answer;
+	@ Column(name=IS_CORRECT)
+	private boolean isCorrect;
+	@ Column(name=QUESTION_ID)
+	private Question question;
+	public Attempt getAttempt() {
+		return attempt;
+	}
+	public void setAttempt(Attempt attempt) {
+		this.attempt = attempt;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public boolean isCorrect() {
+		return isCorrect;
+	}
+	public void setCorrect(boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
+	public Question getQuestion() {
+		return question;
+	}
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+}
