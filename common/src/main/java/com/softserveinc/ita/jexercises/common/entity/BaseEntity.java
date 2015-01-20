f99d8a4a4bebb548964eed1de371db9a3cd32092
tarasvuyiv <tarasvuyiv@github.com>
2015-01-20 13:50:01 +0200

    adding BaseEntity

---
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/BaseEntity.java
@@ -0,0 +1,60 @@
+package com.softserveinc.ita.jexercises.common.entity;
+
+import java.util.Date;
+
+public class BaseEntity {
+	private Long id;
+	private Long createdUser;
+	private Long modifiedUser;
+	private Date createdDate;
+	private Date modifiedDate;
+	private Double version;
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public Long getCreatedUser() {
+		return this.createdUser;
+	}
+	
+	public Long getModifiedUser() {
+		return this.modifiedUser;
+	}
+	
+	public Date getCreatedDate() {
+		return this.createdDate;
+	}
+	
+	public Date getModifiedDate() {
+		return this.modifiedDate;
+	}
+	
+	public Double getVersion() {
+		return this.version;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setCreatedUser(Long createdUser) {
+		this.createdUser = createdUser;
+	}
+	
+	public void setModifiedUser(Long modifiedUser) {
+		this.modifiedUser = modifiedUser;
+	}
+	
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+	public void setVersion(Double version) {
+		this.version = version;
+	}
+}
