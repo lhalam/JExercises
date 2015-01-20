aae5ec2dd4c5f3f462efe57dd89c8831ffd881a4
volodymyrY <v.yakumiv@gmail.com>
2015-01-20 20:20:40 +0200

    fix Question entity and add Test entity

--- common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Question.java
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Question.java
@@ -4,33 +4,27 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name="QUESTION")
+@Table(name = "QUESTION")
 public class Question extends BaseEntity {
 
-
-@Column (name="DESCRIPTION")
+    @Column(name = "DESCRIPTION")
     private String description;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "question_test",  joinColumns = {
-            @JoinColumn(name = "QUESTION_ID", nullable = false, updatable = false) },
-            inverseJoinColumns = { @JoinColumn(name = "TEST_ID",
-                    nullable = false, updatable = false) })
-    private Set<Test> tests ;
+    @ManyToMany(fetch = FetchType.LAZY, targetEntity = Test.class)
+    @JoinTable(name = "QUESTION_TEST", joinColumns = {
+            @JoinColumn(name = "QUESTION_ID", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "TEST_ID",
+                    nullable = false, updatable = false)})
+    private Set<Test> tests;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "question")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "QUESTION")
     private Set<SharedAnswer> sharedAnswers;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "question")
-    private Set<Assert> asserts ;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "QUESTION")
+    private Set<Assert> asserts;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "question")
-    private Set<UserAnswer> userAnswers ;
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "QUESTION")
+    private Set<UserAnswer> userAnswers;
 
     public Long getId() {
         return id;
@@ -44,10 +38,45 @@ public class Question extends BaseEntity {
         return description;
     }
 
-    public Question() {}
-
-    public Question(Long id, String description){
-        this.id=id;
-        this.description=description;
+    public Set<Test> getTests() {
+        return tests;
     }
+
+    public void setTests(Set<Test> tests) {
+        this.tests = tests;
+    }
+
+    public Set<SharedAnswer> getSharedAnswers() {
+        return sharedAnswers;
+    }
+
+    public void setSharedAnswers(Set<SharedAnswer> sharedAnswers) {
+        this.sharedAnswers = sharedAnswers;
+    }
+
+    public Set<Assert> getAsserts() {
+        return asserts;
+    }
+
+    public void setAsserts(Set<Assert> asserts) {
+        this.asserts = asserts;
+    }
+
+    public Set<UserAnswer> getUserAnswers() {
+        return userAnswers;
+    }
+
+    public void setUserAnswers(Set<UserAnswer> userAnswers) {
+        this.userAnswers = userAnswers;
+    }
+
+    public Question() {
+    }
+
+    public Question(Long id, String description) {
+        super();
+        this.id = id;
+        this.description = description;
+    }
+
 }
--- common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Test.java
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Test.java
@@ -4,60 +4,70 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table
-public class Test extends BaseEntity{
-	
-	@Id
-    @GeneratedValue
-	private Long idTest;
-	private String description;
-	private Boolean isPublic;
-	@ManyToMany
-	private Attempt attempt;
-	@ManyToMany
-	private Set<Question> questionSet;
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Boolean getIsPublic() {
-		return isPublic;
-	}
-	public void setIsPublic(Boolean isPublic) {
-		this.isPublic = isPublic;
-	}
-	public Attempt getAttempt() {
-		return attempt;
-	}
-	public void setAttempt(Attempt attempt) {
-		this.attempt = attempt;
-	}
+@Table(name = "TEST")
+public class Test extends BaseEntity {
 
-    public Set<Question> getQuestionSet() {
-		return questionSet;
-	}
-	public void setQuestionSet(Set<Question> questionSet) {
-		this.questionSet = questionSet;
-	}
-	public Test(String description, Boolean isPublic, Attempt attempt,
-			Set questionSet) {
-		super();
-		this.description = description;
-		this.isPublic = isPublic;
-		this.attempt = attempt;
-		this.questionSet = questionSet;
-	}
-	
-	
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "IS_PUBLIC")
+    private Boolean isPublic;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "TEST")
+    private Set<Attempt> attempts;
+
+    @ManyToMany(fetch = FetchType.LAZY, targetEntity = Question.class)
+    @JoinTable(name = "QUESTION_TEST", joinColumns = {
+            @JoinColumn(name = "QUESTION_ID", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "TEST_ID",
+                    nullable = false, updatable = false)})
+    private Set<Question> questions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(Boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public Set<Attempt> getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(Set<Attempt> attempts) {
+        this.attempts = attempts;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
+
+    public Test() {
+
+    }
+
+    public Test(String description, Boolean isPublic) {
+        super();
+        this.description = description;
+        this.isPublic = isPublic;
+
+    }
 
 }
