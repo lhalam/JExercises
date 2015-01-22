9333970fc9df5ff337f11ecbef4497b093ecb2a6
unknown <C:\Documents and Settings\\Application Data\The Bat!>
2015-01-22 15:23:42 +0200

    fix attempt

--- common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Attempt.java
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/Attempt.java
@@ -1,7 +1,9 @@
 package com.softserveinc.ita.jexercises.common.entity;
 
 import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,47 +12,51 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "ATTEMPT")
 public class Attempt extends BaseEntity {
-	
-	@ManyToOne
-	@Column(name = "USER_ID")
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "USER_ID")
 	private User user;
-	
-	@ManyToOne (fetch = FetchType.LAZY, targetEntity = Test.class)
-    @JoinColumn(name = "attempts")
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "TEST_ID")
 	private Test test;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy="attempt")
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "attempt")
 	@Column(name = "USER_ANSWER_ID")
-	private HashSet<UserAnswer> userAnswer = new HashSet<UserAnswer>();
-	
-	public Attempt() {}
-	
+	private Set<UserAnswer> userAnswers;
+
+	public Attempt() {
+	}
+
 	public Attempt(User user, Test test) {
 		this.user = user;
 		this.test = test;
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public Test getTest() {
 		return test;
 	}
+
 	public void setTest(Test test) {
 		this.test = test;
 	}
-	public HashSet<UserAnswer> getUserAnswer() {
-		return userAnswer;
-	}
-	public void setUserAnswer(HashSet<UserAnswer> userAnswer) {
-		this.userAnswer = userAnswer;
+
+	public Set<UserAnswer> getUserAnswers() {
+		return userAnswers;
 	}
 
+	public void setUserAnswer(Set<UserAnswer> userAnswers) {
+		this.userAnswers = userAnswers;
+	}
 }
