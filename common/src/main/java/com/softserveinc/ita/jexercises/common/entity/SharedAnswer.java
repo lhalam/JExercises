1c81a610c05e1dd710f9bc107d209ba67de817d9
volodymyrY <v.yakumiv@gmail.com>
2015-01-22 14:37:22 +0200

    fix SharedAnswer

--- common/src/main/java/com/softserveinc/ita/jexercises/common/entity/SharedAnswer.java
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/SharedAnswer.java
@@ -2,48 +2,53 @@ package com.softserveinc.ita.jexercises.common.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "SHARED_ANSWER")
 public class SharedAnswer extends BaseEntity {
-	
-	@Column(name = "COMMENT")
-	private String comment;
-	
-	@Column(name = "PARENT_ID")
-	private Long parentId;
-	
-	@ManyToOne
-	@Column(name = "QUESTION_ID")
-	private Question question;
-	
-	public SharedAnswer() {}
-	
-	public SharedAnswer(String comment, Long parentId) {
-		this.comment = comment;
-		this.parentId = parentId;
-	}
-	
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public Long getParentId() {
-		return parentId;
-	}
-	public void setParentId(Long parentId) {
-		this.parentId = parentId;
-	}
-	public Question getQuestion() {
-		return question;
-	}
-	public void setQuestion(Question question) {
-		this.question = question;
-	}
-	
 
+    @Column(name = "COMMENT")
+    private String comment;
+
+    @Column(name = "PARENT_ID")
+    private Long parentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    public SharedAnswer() {
+    }
+
+    public SharedAnswer(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
