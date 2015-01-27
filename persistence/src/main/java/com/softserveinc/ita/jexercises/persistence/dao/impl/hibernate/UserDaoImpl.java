c7bfd95e3a2f59d94c3662aaa7895d34e70cbd97
Oksana Senchuk <senchuk.oksana@gmail.com>
2015-01-27 21:03:42 +0200

    modify UserDaoImpl, UserServiceImpl classes(add annotations);

--- persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/hibernate/UserDaoImpl.java
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/hibernate/UserDaoImpl.java
@@ -1,13 +1,16 @@
 package com.softserveinc.ita.jexercises.persistence.dao.impl.hibernate;
 
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
 import com.softserveinc.ita.jexercises.common.entity.User;
 import com.softserveinc.ita.jexercises.persistence.dao.UserDao;
 
+@Repository
 public class UserDaoImpl extends GenericDaoImpl<User, Long> implements UserDao {
 
     @Override
-    public User findByEmail(String email) {
+    public User findByEmail(String email) throws NoResultException {
 	Query q = entityManager.createQuery(
 		"select u from User u where u.email=:email", User.class)
 		.setParameter("email", email);
