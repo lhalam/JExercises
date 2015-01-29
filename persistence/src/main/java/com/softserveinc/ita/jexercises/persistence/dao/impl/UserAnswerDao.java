29cdab6d7b86b3adfa02f016186161a021f07f25
olko123 <olegpavlish100@gmail.com>
2015-01-29 20:51:04 +0200

    Changed UserAnswer DAO to fit checkstyle requirements

---
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/UserAnswerDao.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.jexercises.persistence.dao.impl;
+
+import com.softserveinc.ita.jexercises.common.entity.UserAnswer;
+import com.softserveinc.ita.jexercises.persistence.dao.GenericDao;
+
+/**
+ * DAO of UserAnswer
+ * 
+ * @author Oleg Pavlish
+ * @version 1.0
+ */
+public interface UserAnswerDao extends GenericDao<UserAnswer, Long> {
+}
