c24084ebd1add1fb47a55849bdb292efa8ecf4ca
tarasvuyiv <tarasvuyiv@github.com>
2015-01-29 12:53:09 +0200

    update BaseEntity and GenericDao

--- common/src/main/java/com/softserveinc/ita/jexercises/common/entity/BaseEntity.java
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/BaseEntity.java
@@ -9,77 +9,101 @@ import javax.persistence.Id;
 import javax.persistence.Version;
 import javax.persistence.MappedSuperclass;
 
+/**
+ * Represents Base Entity - parent of all entities. Contains common fields.
+ * 
+ * @author Taras Vuyiv
+ *
+ */
 @MappedSuperclass
 public abstract class BaseEntity {
-	
-	@Id
-	@GeneratedValue
-	@Column(name = "ID")
-	@Basic(optional = false)
-	private Long id;
-	
-	@Column(name = "CREATED_BY_USER")
-	private Long createdByUser;
-	
-	@Column(name = "MODIFIED_BY_USER")
-	private Long modifiedByUser;
-	
-	@Column(name = "CREATED_DATE")
-	private Date createdDate;
-	
-	@Column(name = "MODIFIED_DATE")
-	private Date modifiedDate;
-	
-	@Column(name = "VERSION")
-	@Version
-	private Long version;
 
-	public Long getId() {
-		return id;
-	}
+    /**
+     * ID key of entity.
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    @Basic(optional = false)
+    private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    /**
+     * ID of user that created a record in database.
+     */
+    @Column(name = "CREATED_BY_USER")
+    private Long createdByUser;
 
-	public Long getCreatedByUser() {
-		return createdByUser;
-	}
+    /**
+     * ID of user that modified a record in database.
+     */
+    @Column(name = "MODIFIED_BY_USER")
+    private Long modifiedByUser;
 
-	public void setCreatedByUser(Long createdByUser) {
-		this.createdByUser = createdByUser;
-	}
+    /**
+     * Date a record in database has been created.
+     */
+    @Column(name = "CREATED_DATE")
+    private Date createdDate;
 
-	public Long getModifiedByUser() {
-		return modifiedByUser;
-	}
+    /**
+     * Date a record in database has been modified.
+     */
+    @Column(name = "MODIFIED_DATE")
+    private Date modifiedDate;
 
-	public void setModifiedByUser(Long modifiedByUser) {
-		this.modifiedByUser = modifiedByUser;
-	}
+    /**
+     * Version of the record.
+     */
+    @Column(name = "VERSION")
+    @Version
+    private Long version;
 
-	public Date getCreatedDate() {
-		return createdDate;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
+    public Long getCreatedByUser() {
+        return createdByUser;
+    }
 
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
+    public void setCreatedByUser(Long createdByUser) {
+        this.createdByUser = createdByUser;
+    }
 
-	public Long getVersion() {
-		return version;
-	}
+    public Long getModifiedByUser() {
+        return modifiedByUser;
+    }
 
-	public void setVersion(Long version) {
-		this.version = version;
-	}
-	
+    public void setModifiedByUser(Long modifiedByUser) {
+        this.modifiedByUser = modifiedByUser;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
 }
---
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/GenericDao.java
@@ -0,0 +1,67 @@
+package com.softserveinc.ita.jexercises.persistence.dao;
+
+import java.util.List;
+import java.io.Serializable;
+
+/**
+ * Represents interface for generic DAO. Parent interface for all DAO
+ * interfaces.
+ * 
+ * @author Taras Vuyiv
+ *
+ * @param <T>
+ *            Entity class.
+ * @param <PK>
+ *            Type of entity key.
+ */
+public interface GenericDao<T, PK extends Serializable> {
+    /**
+     * Create record in database.
+     * 
+     * @param object
+     *            Entity object.
+     * @return Entity object.
+     */
+    T create(T object);
+
+    /**
+     * Update record in database.
+     * 
+     * @param object
+     *            Entity object.
+     * @return Entity object.
+     */
+    T update(T object);
+
+    /**
+     * Delete record in database.
+     * 
+     * @param object
+     *            Entity object.
+     */
+    void delete(T object);
+
+    /**
+     * Delete record by ID.
+     * 
+     * @param id
+     *            ID of record should be deleted.
+     */
+    void deleteById(PK id);
+
+    /**
+     * Find a record by ID.
+     * 
+     * @param id
+     *            ID of record should be found.
+     * @return Entity object.
+     */
+    T findById(PK id);
+
+    /**
+     * Find all records.
+     * 
+     * @return List of entity objects.
+     */
+    List<T> findAll();
+}
