1eada51e117879f8f38bbfce92e953355e975384
RPhysicist <demkovych_igor@meta.ua>
2015-01-29 23:14:21 +0200

    update TestDao

---
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/TestDao.java
@@ -0,0 +1,23 @@
+package com.softserveinc.ita.jexercises.persistence.dao.impl;
+
+import com.softserveinc.ita.jexercises.common.entity.Test;
+ 
+import java.util.List;
+
+/**
+ * DAO of test.
+ *
+ * @author Ihor Demkovych
+ * @version 1.0
+ */
+public interface TestDao extends GenericDao<Test, Long> {
+
+    /**
+     * Searching tests by question id.
+     *
+     * @param questionId Question id.
+     * @return Set of tests if exists or null if question does
+     * not have any tests
+     */
+    List<Test> findAllByQuestionId (Long questionId);
+}
