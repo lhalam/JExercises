ef94a621d28d77b60b6f508a17a38f25543c7372
olko123 <olegpavlish100@gmail.com>
2015-01-29 20:53:57 +0200

    Changed SharedAnswer o fit checkstyle requirements

--- common/src/main/java/com/softserveinc/ita/jexercises/common/entity/SharedAnswer.java
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/entity/SharedAnswer.java
@@ -7,23 +7,47 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+/**
+ * Represents SharedAnswer Entity
+ * 
+ * @author Oleg Pavlish
+ * @version 1.0
+ */
 @Entity
 @Table(name = "SHARED_ANSWER")
 public class SharedAnswer extends BaseEntity {
 
+    /**
+     * Comment on the question
+     */
     @Column(name = "COMMENT")
     private String comment;
 
+    /**
+     * Id of parent comment node
+     */
     @Column(name = "PARENT_ID")
     private Long parentId;
 
+    /**
+     * Commented question
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "QUESTION_ID")
     private Question question;
 
+    /**
+     * Default constructor providing new instance of sharedAnswer
+     */
     public SharedAnswer() {
     }
 
+    /**
+     * Constructor providing new instance of sharedAnswer
+     * 
+     * @param comment
+     *            Coment on question
+     */
     public SharedAnswer(String comment) {
         this.comment = comment;
     }
