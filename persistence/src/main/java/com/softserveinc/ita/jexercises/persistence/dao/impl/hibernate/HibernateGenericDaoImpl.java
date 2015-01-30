d079711fb3a1d2b27c44e75a3958ef8d7dd4a56d
tarasvuyiv <tarasvuyiv@github.com>
2015-01-30 10:07:09 +0200

    update GenericDaoImpl

--- persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/hibernate/HibernateGenericDaoImpl.java
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/hibernate/HibernateGenericDaoImpl.java
@@ -11,77 +11,110 @@ import javax.persistence.Query;
 
 import com.softserveinc.ita.jexercises.persistence.dao.GenericDao;
 
+/**
+ * Represents implementation of GenericDao interface.
+ * 
+ * @author Taras Vuyiv
+ *
+ * @param <T>
+ *            Entity class.
+ * @param <PK>
+ *            ID key type.
+ */
+
 public class HibernateGenericDaoImpl<T, PK extends Serializable> implements
-		GenericDao<T, PK> {
+        GenericDao<T, PK> {
+    /**
+     * Service final variable for getEntityName() method.
+     */
+    private static final String EMPTY = "";
 
-	private Class<T> entityClass;
+    /**
+     * Represents entity class.
+     */
+    private Class<T> entityClass;
 
-	@PersistenceContext
-	private EntityManager entityManager;
-	private String entity;
+    /**
+     * Entity manager variable.
+     */
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    /**
+     * Service variable for getEntityName() method.
+     */
+    private String entity;
 
-	public HibernateGenericDaoImpl() {
-		ParameterizedType genericSuperclass = (ParameterizedType) getClass()
-				.getGenericSuperclass();
-		entityClass = (Class) genericSuperclass.getActualTypeArguments()[0];
-	}
+    /**
+     * Constructor. Assigns class of entity.
+     */
+    public HibernateGenericDaoImpl() {
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass()
+                .getGenericSuperclass();
+        entityClass = (Class) genericSuperclass.getActualTypeArguments()[0];
+    }
 
-	@Override
-	public T create(T object) {
-		this.entityManager.persist(object);
-		return object;
-	}
+    @Override
+    public T create(T object) {
+        this.entityManager.persist(object);
+        return object;
+    }
 
-	@Override
-	public T findById(PK id) {
-		return this.entityManager.find(entityClass, id);
-	}
+    @Override
+    public T findById(PK id) {
+        return this.entityManager.find(entityClass, id);
+    }
 
-	@Override
-	public void deleteById(PK id) {
-		T object = findById(id);
-		this.entityManager.remove(object);
-	}
+    @Override
+    public void deleteById(PK id) {
+        T object = findById(id);
+        this.entityManager.remove(object);
+    }
 
-	@Override
-	public T update(T object) {
-		return this.entityManager.merge(object);
-	}
+    @Override
+    public T update(T object) {
+        return this.entityManager.merge(object);
+    }
 
-	@Override
-	public void delete(T object) {
-		object = this.entityManager.merge(object);
-		this.entityManager.remove(object);
-	}
+    @Override
+    public void delete(T object) {
+        this.entityManager.remove(object);
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<T> findAll() {
-		Query query = entityManager.createQuery("select x from "
-				+ getEntityName() + " x");
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<T> findAll() {
+        Query query = entityManager.createQuery("select x from "
+                + getEntityName() + " x");
 
-		return (List<T>) query.getResultList();
-	}
+        return (List<T>) query.getResultList();
+    }
 
-	public String getEntityName() {
-		if (entity == null) {
-			Entity entityAnn = (Entity) entityClass.getAnnotation(Entity.class);
+    /**
+     * Get name of entity class.
+     * 
+     * @return String with the name of entity class.
+     */
+    private String getEntityName() {
+        if (entity == null) {
+            Entity entityAnn = (Entity) entityClass.getAnnotation(Entity.class);
 
-			if (entityAnn != null && !entityAnn.name().equals("")) {
-				entity = entityAnn.name();
-			} else {
-				entity = entityClass.getSimpleName();
-			}
-		}
+            if (entityAnn != null && !entityAnn.name().equals(EMPTY)) {
+                entity = entityAnn.name();
+            } else {
+                entity = entityClass.getSimpleName();
+            }
+        }
 
-		return entity;
-	}
+        return entity;
+    }
 
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
 
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
+    protected void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
 }
