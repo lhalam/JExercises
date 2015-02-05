b09a3bf064a2ef9924f7aa06fbc5f540ec6eadcb
tarasvuyiv <tarasvuyiv@github.com>
2015-02-05 13:37:46 +0200

    adding JavaDoc for GenericDao

--- persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/GenericDao.java
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/GenericDao.java
@@ -1,71 +1,69 @@
 package com.softserveinc.ita.jexercises.persistence.dao;
 
-import java.util.List;
-import java.io.Serializable;
-
 import com.softserveinc.ita.jexercises.common.dto.SearchCondition;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Represents interface for generic DAO. Parent interface for all DAO
  * interfaces.
- * 
- * @author Taras Vuyiv
  *
- * @param <T>
- *            Entity class.
- * @param <PK>
- *            Type of entity key.
+ * @param <T>  Entity class.
+ * @param <PK> Type of entity key.
+ * @author Taras Vuyiv
  */
 public interface GenericDao<T, PK extends Serializable> {
     /**
      * Create record in database.
-     * 
-     * @param object
-     *            Entity object.
+     *
+     * @param object Entity object.
      * @return Entity object.
      */
     T create(T object);
 
     /**
      * Update record in database.
-     * 
-     * @param object
-     *            Entity object.
+     *
+     * @param object Entity object.
      * @return Entity object.
      */
     T update(T object);
 
     /**
      * Delete record in database.
-     * 
-     * @param object
-     *            Entity object.
+     *
+     * @param object Entity object.
      */
     void delete(T object);
 
     /**
      * Delete record by ID.
-     * 
-     * @param id
-     *            ID of record should be deleted.
+     *
+     * @param id ID of record should be deleted.
      */
     void deleteById(PK id);
 
     /**
      * Find a record by ID.
-     * 
-     * @param id
-     *            ID of record should be found.
+     *
+     * @param id ID of record should be found.
      * @return Entity object.
      */
     T findById(PK id);
 
     /**
      * Find all records.
-     * 
+     *
      * @return List of entity objects.
      */
     List<T> findAll();
-    
+
+    /**
+     * Method for sorting, filtering and paging.
+     *
+     * @param searchCondition Object with search parameters.
+     * @return List of entity objects.
+     */
     List<T> findAllByCriteria(SearchCondition searchCondition);
 }
