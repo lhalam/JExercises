cef2bea79935dd46323c7c3147675474ac136781
Kucheryavenko Dmytro <kucheryavenko.dmtr@gmail.com>
2015-02-16 13:59:33 +0200

    Add IndexController

---
+++ web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/IndexController.java
@@ -0,0 +1,25 @@
+package com.softserveinc.ita.jexercises.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Class which includes index-controller
+ * 
+ * @author Kucheryavenko Dmytro
+ *
+ * @version 1.0
+ */
+@Controller
+public class IndexController {
+    /**
+     * Method provides mapping on "idex" input
+     * 
+     * @return index.jsp
+     */
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String printWelcome() {
+        return "index";
+    }
+}
