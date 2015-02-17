302284906681f10d817d6d94f460c1d1be9bd553
Rphysicist <demkovych_igor@meta.ua>
2015-02-17 16:42:06 +0200

    Fix errors

--- web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/TestGridController.java
+++ web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/TestGridController.java
@@ -1,12 +1,6 @@
 package com.softserveinc.ita.jexercises.web.controllers;
 
-import com.softserveinc.ita.jexercises.business.services.UserRegistrationService;
-import com.softserveinc.ita.jexercises.business.utils.EmailExistsException;
-import com.softserveinc.ita.jexercises.common.dto.TestGridDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
