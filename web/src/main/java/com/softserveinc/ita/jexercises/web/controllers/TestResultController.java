6c0f273655613bbd6437ea4ffa7d7b9acb9cd7de
unknown <C:\Documents and Settings\\Application Data\The Bat!>
2015-02-21 00:33:11 +0200

    add import on controller

--- web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/TestResultController.java
+++ web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/TestResultController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.softserveinc.ita.jexercises.business.services.TestResultService;
