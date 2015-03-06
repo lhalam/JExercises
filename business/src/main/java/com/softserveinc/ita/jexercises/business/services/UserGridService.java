59b9378df3a300f7e0ada4dfe5e210ef9c988d18
tarasvuyiv <tarasvuyiv@github.com>
2015-03-06 21:43:36 +0200

    add User Grid Service

---
+++ business/src/main/java/com/softserveinc/ita/jexercises/business/services/UserGridService.java
@@ -0,0 +1,22 @@
+package com.softserveinc.ita.jexercises.business.services;
+
+import com.softserveinc.ita.jexercises.common.dto.GridResponseDto;
+import com.softserveinc.ita.jexercises.common.dto.SearchCondition;
+import com.softserveinc.ita.jexercises.common.dto.UserProfileDto;
+
+/**
+ * Services used for users DataTable.
+ *
+ * @author Taras Vuyiv
+ * @version 1.0
+ */
+public interface UserGridService {
+    /**
+     * Getting data for users grid.
+     *
+     * @param searchCondition Filter parameters.
+     * @return Grid Response DTO with users profile info.
+     */
+    GridResponseDto<UserProfileDto> getGridRows(
+            SearchCondition searchCondition);
+}
---
+++ business/src/main/java/com/softserveinc/ita/jexercises/business/services/impl/UserGridServiceImpl.java
@@ -0,0 +1,49 @@
+package com.softserveinc.ita.jexercises.business.services.impl;
+
+import com.softserveinc.ita.jexercises.business.services.UserGridService;
+import com.softserveinc.ita.jexercises.business.services.UserProfileService;
+import com.softserveinc.ita.jexercises.common.dto.GridResponseDto;
+import com.softserveinc.ita.jexercises.common.dto.SearchCondition;
+import com.softserveinc.ita.jexercises.common.dto.UserProfileDto;
+import com.softserveinc.ita.jexercises.common.entity.User;
+import com.softserveinc.ita.jexercises.persistence.dao.impl.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents UserGridService interface implementation.
+ *
+ * @author Taras Vuyiv
+ */
+@Service
+public class UserGridServiceImpl implements UserGridService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private UserProfileService userProfileService;
+
+    @Override
+    public GridResponseDto<UserProfileDto> getGridRows(SearchCondition searchCondition) {
+        GridResponseDto<UserProfileDto> response = new GridResponseDto<>();
+
+        response.setDraw(searchCondition.getDraw());
+        response.setRecordsFiltered(userDao.getNumberOfFilteredRecords(searchCondition));
+        response.setRecordsTotal(userDao.getNumberOfRecords());
+
+        List<User> userList = userDao.findAllByCriteria(searchCondition);
+        List<UserProfileDto> userProfileDtoList = new ArrayList<>();
+
+        for (User user : userList) {
+            userProfileDtoList.add(userProfileService.getUserInfo(user));
+        }
+
+        response.setData(userProfileDtoList);
+
+        return response;
+    }
+}
