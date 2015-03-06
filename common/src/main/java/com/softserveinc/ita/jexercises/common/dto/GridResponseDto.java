225c62694cc0e0931af338b445de831292a4fd71
tarasvuyiv <tarasvuyiv@github.com>
2015-03-06 21:41:56 +0200

    add Grid Response DTO

---
+++ common/src/main/java/com/softserveinc/ita/jexercises/common/dto/GridResponseDto.java
@@ -0,0 +1,48 @@
+package com.softserveinc.ita.jexercises.common.dto;
+
+import java.util.List;
+
+/**
+ * Data Transfer Object used to return data to DataTables.
+ *
+ * @param <T> Class of data to be returned.
+ * @author Taras Vuyiv.
+ */
+public class GridResponseDto<T> {
+    private int draw;
+    private Long recordsTotal;
+    private Long recordsFiltered;
+    private List<T> data;
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public Long getRecordsTotal() {
+        return recordsTotal;
+    }
+
+    public void setRecordsTotal(Long recordsTotal) {
+        this.recordsTotal = recordsTotal;
+    }
+
+    public Long getRecordsFiltered() {
+        return recordsFiltered;
+    }
+
+    public void setRecordsFiltered(Long recordsFiltered) {
+        this.recordsFiltered = recordsFiltered;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
