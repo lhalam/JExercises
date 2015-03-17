2ca889417e7c166737526631d0e2680fbed80d11
Kucheryavenko Dmytro <kucheryavenko.dmtr@gmail.com>
2015-03-17 12:27:14 +0200

    edit questioncreating.js, questioncreating.jsp

--- web/src/main/webapp/resources/js/questioncreating.js
+++ web/src/main/webapp/resources/js/questioncreating.js
@@ -10,10 +10,6 @@ function Assert(expectedAnswer, inputData) {
 	this.expectedAnswer = expectedAnswer;
 	this.inputData = inputData
 }
-/*
- * var assertOb1 = { expectedAnswer : "some Answ", inputData : "some InpD" };
- * var assertOb2 = { expectedAnswer : "some Answ2", inputData : "some InpD2" };
- */
 
 var assertDto = [];
 
@@ -23,22 +19,8 @@ var table;
 
 function keypressHandler(e) {
 	if (e.keyCode == 13) {
-		// var text=$(this).val("input_data")?json['assertDtoList'].push({});
-
 		sibling = $(this);
 		$(this).find("textarea").blur();
-		// var ob1=new Assert("answ1","data1");
-		// var ob2=new Assert("answ2","data2");
-		assertDto.push(new Assert("answasdasd", "dataasdas"));
-		assertDto.push(new Assert("answer", "datadd"));
-		// $("td.odd").
-		/*
-		 * $("td").each(function() { var str =
-		 * $("textarea.no-expand.edit_mode").val(); console.log(str); });
-		 */
-		// alert(str);
-		// json['assertDtoList'].push({});
-		// console.log($( this ));
 	}
 
 }
@@ -87,9 +69,9 @@ function onTDClickHandler() {
 }
 $(document).ready(
 		function() {
-			table = $('#assert').DataTable({// create table
+			table = $('#assert').DataTable({
 				"columnDefs" : [ {
-					"width" : "50%",// set first column width
+					"width" : "50%",
 					"targets" : 0
 				} ]
 			});
@@ -104,10 +86,8 @@ $(document).ready(
 					}
 					if(index%2==1){
 						assertDto.push(new Assert(str1,str2));
-						//console.log(assertDto);
 					}
 				});
-				console.log(assertDto);
 			});
 
 			$("#addRow").click(
@@ -124,21 +104,14 @@ $(document).ready(
 
 			$("#submitButton").click(
 					function(event) {
-						// var dataRequest = {
-						// questionName : '0',
-						// questionDescription : '0',
-						// assertDtoList : assertDto
-						// };
 						var dr = new dataRequest(assertDto);
 						dr.questionName = $('#questionName').code().toString();
 						dr.questionDescription = $('#questionDescription')
 								.code().toString();
-						// assertDtoList = assertDto;
 						$.ajax({
 							url : "/web/questioncreating",
 							type : 'POST',
 							dataType : 'html',
-							// data : JSON.stringify(dataRequest),
 							data : JSON.stringify(dr),
 							contentType : 'application/json',
 							mimeType : 'application/json',
