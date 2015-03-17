369d9cf3c6207026a90c636b2b78f08da03c140c
Kucheryavenko Dmytro <kucheryavenko.dmtr@gmail.com>
2015-03-17 13:36:06 +0200

    edit:AssertDao, HibernateAssertDaoImpl

--- persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/AssertDao.java
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/AssertDao.java
@@ -1,5 +1,7 @@
 package com.softserveinc.ita.jexercises.persistence.dao.impl;
 
+import java.util.List;
+
 import com.softserveinc.ita.jexercises.common.entity.Assert;
 import com.softserveinc.ita.jexercises.persistence.dao.GenericDao;
 
@@ -11,4 +13,12 @@ import com.softserveinc.ita.jexercises.persistence.dao.GenericDao;
  * @version 1.0
  */
 public interface AssertDao extends GenericDao<Assert, Long> {
+    /**
+     * Searching all asserts by question id.
+     * 
+     * @param questionId
+     *            Question id.
+     * @return Set of all asserts.
+     */
+    List<Assert> findAllByQuestionId(Long questionId);
 }
--- persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/hibernate/HibernateAssertDaoImpl.java
+++ persistence/src/main/java/com/softserveinc/ita/jexercises/persistence/dao/impl/hibernate/HibernateAssertDaoImpl.java
@@ -1,7 +1,14 @@
 package com.softserveinc.ita.jexercises.persistence.dao.impl.hibernate;
 
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
+
 import com.softserveinc.ita.jexercises.common.entity.Assert;
+
 import com.softserveinc.ita.jexercises.persistence.dao.impl.AssertDao;
 
 /**
@@ -14,4 +21,19 @@ import com.softserveinc.ita.jexercises.persistence.dao.impl.AssertDao;
 @Repository
 public class HibernateAssertDaoImpl extends
         HibernateGenericDaoImpl<Assert, Long> implements AssertDao {
+
+    @Override
+    public List<Assert> findAllByQuestionId(Long questionId) {
+        try {
+            String squerty = "select distinct ua from Assert ua "
+                    + "where ua.question.id=:questionId";
+            Query q = getEntityManager().createQuery(squerty);
+            q.setParameter("questionId", questionId);
+            @SuppressWarnings("unchecked")
+            List<Assert> asserts = (List<Assert>) q.getResultList();
+            return asserts;
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
