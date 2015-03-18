9d452a78d3e2657f82e74d256a36fb41b596df68
Kucheryavenko Dmytro <kucheryavenko.dmtr@gmail.com>
2015-03-18 10:56:18 +0200

    edit javadoc

--- web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/QuestionCreatingController.java
+++ web/src/main/java/com/softserveinc/ita/jexercises/web/controllers/QuestionCreatingController.java
@@ -1,10 +1,10 @@
 package com.softserveinc.ita.jexercises.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
